fd9ee6eace69d0b7a79b54e084224e7b5ce202ed
Jamie Bliss <jamie@ivyleav.es>
2019-08-29 18:09:26 -0400

    Add some pytest stuff

---
+++ tests/test_gql.py
@@ -0,0 +1,18 @@
+import gqlmod  # noqa
+import testmod.queries
+
+
+def test_names():
+    qnames = {name for name in dir(testmod.queries) if not name.startswith('_')}
+    assert qnames == {'Hero', 'HeroComparison', 'HeroForEpisode', 'HeroNameAndFriends'}
+    assert all(callable(getattr(testmod.queries, name)) for name in qnames)
+
+
+def test_data():
+    result = testmod.queries.HeroNameAndFriends()
+    assert not result.errors
+    assert result.data == {
+        'hero': {'friends': [{'name': 'Luke Skywalker'},
+                             {'name': 'Han Solo'},
+                             {'name': 'Leia Organa'}],
+                 'name': 'R2-D2'}}
---
+++ tests/test_providers.py
@@ -0,0 +1,10 @@
+from gqlmod.providers import get_provider, with_provider
+
+def test_provider_change():
+    orig = get_provider('starwars')
+    with with_provider('starwars'):
+        new = get_provider('starwars')
+        assert orig is not new
+
+    old = get_provider('starwars')
+    assert old is new
