08931d56c70d4695cfab8706ea0a151927c33de6
Jamie Bliss <jamie@ivyleav.es>
2019-08-30 21:27:52 -0400

    Use pytest from setup.py test

--- setup.py
+++ setup.py
@@ -1,3 +1,28 @@
-from setuptools import setup
+import sys
 
-setup()
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    cmdclass={'test': PyTest},
+)
