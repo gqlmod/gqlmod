e77ad539ba1e0cd6fd868236844bf42869bc0e48
Jamie Bliss <jamie@ivyleav.es>
2019-08-30 21:22:00 -0400

    Fix test_provider_change

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -1,5 +1,6 @@
 from gqlmod.providers import get_provider, with_provider
 
+
 def test_provider_change():
     orig = get_provider('starwars')
     with with_provider('starwars'):
@@ -7,4 +8,4 @@ def test_provider_change():
         assert orig is not new
 
     old = get_provider('starwars')
-    assert old is new
+    assert old is orig
